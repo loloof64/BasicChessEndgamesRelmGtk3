c9d544b3a89736943159f31ec77f63d14653b816
loloof64 <laurent.bernabe@gmail.com>
2022-08-15 20:57:59 +0200

    Bug fixed: window's background was strange on resize.

--- src/widgets/mainwindow.rs
+++ src/widgets/mainwindow.rs
@@ -1,4 +1,4 @@
-use gtk::prelude::{WidgetExt};
+use gtk::prelude::WidgetExt;
 use relm::Widget;
 use relm_derive::{widget, Msg};
 
@@ -7,18 +7,21 @@ use super::chessboard::ChessBoard;
 #[widget]
 impl Widget for MainWindow {
     view! {
+        #[name="root"]
         gtk::Window {
             #[name="board"]
             ChessBoard {
 
             },
-            delete_event(_,_) => (Quit, gtk::Inhibit(false))
+            delete_event(_window, _event) => (Quit, gtk::Inhibit(false)),
+            size_allocate(_window, _allocation) => Resize,
         }
     }
 
     fn update(&mut self, event: Msg) {
         match event {
             Quit => gtk::main_quit(),
+            Resize => self.update_on_resize(),
         }
     }
 
@@ -31,9 +34,16 @@ impl Widget for MainWindow {
     }
 }
 
+impl MainWindow {
+    fn update_on_resize(&mut self) {
+        self.widgets.root.queue_draw();
+    }
+}
+
 #[derive(Msg)]
 pub enum Msg {
     Quit,
+    Resize,
 }
 
 pub struct Model {}
