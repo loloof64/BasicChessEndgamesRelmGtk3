79863476962413eb29f578ce462594ebc6ff49e2
loloof64 <laurent.bernabe@gmail.com>
2022-08-17 23:27:23 +0200

    Refactoring chess board component

--- src/widgets/chessboard/mod.rs
+++ src/widgets/chessboard/mod.rs
@@ -77,83 +77,13 @@ impl Widget for ChessBoard {
                 self.draw().unwrap();
             }
             ButtonDown(event) => {
-                let (x, y) = event.position();
-                let cells_size = self.common_size() as f64 * 0.111;
-                let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
-                let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
-                let file = if self.model.reversed { 7 - col } else { col };
-                let rank = if self.model.reversed { row } else { 7 - row };
-
-                let in_bounds = file >= 0 && file <= 7 && rank >= 0 && rank <= 7;
-                if in_bounds {
-                    let square_index = file as u8 + 8 * rank as u8;
-                    let square = SQ::from(square_index);
-                    let piece = self.model.board.piece_at_sq(square);
-
-                    let not_empty_piece = piece != Piece::None;
-                    let white_turn = self.model.board.turn() == Player::White;
-                    let our_piece = is_side_piece(piece, white_turn);
-
-                    if not_empty_piece && our_piece {
-                        let piece = get_piece_type_from(piece);
-                        let drag_drop_data = DragAndDropData {
-                            piece,
-                            x,
-                            y,
-                            start_file: file as u8,
-                            start_rank: rank as u8,
-                            target_file: file as u8,
-                            target_rank: rank as u8,
-                        };
-                        self.model.dnd_data = Some(drag_drop_data);
-                    }
-                }
+                self.handle_button_down(event);
             }
             ButtonUp(event) => {
-                let (x, y) = event.position();
-                let cells_size = self.common_size() as f64 * 0.111;
-                let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
-                let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
-                let file = if self.model.reversed { 7 - col } else { col };
-                let rank = if self.model.reversed { row } else { 7 - row };
-
-                if self.model.dnd_data.is_some() {
-                    let start_file = self.model.dnd_data.as_ref().unwrap().start_file;
-                    let start_rank = self.model.dnd_data.as_ref().unwrap().start_rank;
-
-                    let start_square_index = start_file + 8 * start_rank;
-                    let start_square = SQ::from(start_square_index);
-                    let target_square_index = (file + 8 * rank) as u8;
-                    let target_square = SQ::from(target_square_index);
-
-                    let uci_move = get_uci_move_for(start_square, target_square, None);
-                    self.model.board.apply_uci_move(&uci_move);
-                }
-
-                self.model.dnd_data = None;
+                self.handle_button_up(event);
             }
             MouseMoved(event) => {
-                let (x, y) = event.position();
-                let cells_size = self.common_size() as f64 * 0.111;
-                let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
-                let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
-                let file = if self.model.reversed { 7 - col } else { col };
-                let rank = if self.model.reversed { row } else { 7 - row };
-
-                match self.model.dnd_data {
-                    Some(ref mut dnd_data) => {
-                        dnd_data.x = x;
-                        dnd_data.y = y;
-
-                        let in_bounds = file >= 0 && file <= 7 && rank >= 0 && rank <= 7;
-
-                        if in_bounds {
-                            dnd_data.target_file = file as u8;
-                            dnd_data.target_rank = rank as u8;
-                        }
-                    }
-                    _ => {}
-                };
+                self.handle_mouse_drag(event);
             }
         }
     }
@@ -216,6 +146,88 @@ impl ChessBoard {
 
         self.set_image(&image)?;
         Ok(())
+    }
+
+    fn handle_button_down(&mut self, event: EventButton) {
+        let (x, y) = event.position();
+        let cells_size = self.common_size() as f64 * 0.111;
+        let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
+        let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
+        let file = if self.model.reversed { 7 - col } else { col };
+        let rank = if self.model.reversed { row } else { 7 - row };
+
+        let in_bounds = file >= 0 && file <= 7 && rank >= 0 && rank <= 7;
+        if in_bounds {
+            let square_index = file as u8 + 8 * rank as u8;
+            let square = SQ::from(square_index);
+            let piece = self.model.board.piece_at_sq(square);
+
+            let not_empty_piece = piece != Piece::None;
+            let white_turn = self.model.board.turn() == Player::White;
+            let our_piece = is_side_piece(piece, white_turn);
+
+            if not_empty_piece && our_piece {
+                let piece = get_piece_type_from(piece);
+                let drag_drop_data = DragAndDropData {
+                    piece,
+                    x,
+                    y,
+                    start_file: file as u8,
+                    start_rank: rank as u8,
+                    target_file: file as u8,
+                    target_rank: rank as u8,
+                };
+                self.model.dnd_data = Some(drag_drop_data);
+            }
+        }
+    }
+
+    fn handle_button_up(&mut self, event: EventButton) {
+        let (x, y) = event.position();
+        let cells_size = self.common_size() as f64 * 0.111;
+        let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
+        let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
+        let file = if self.model.reversed { 7 - col } else { col };
+        let rank = if self.model.reversed { row } else { 7 - row };
+
+        if self.model.dnd_data.is_some() {
+            let start_file = self.model.dnd_data.as_ref().unwrap().start_file;
+            let start_rank = self.model.dnd_data.as_ref().unwrap().start_rank;
+
+            let start_square_index = start_file + 8 * start_rank;
+            let start_square = SQ::from(start_square_index);
+            let target_square_index = (file + 8 * rank) as u8;
+            let target_square = SQ::from(target_square_index);
+
+            let uci_move = get_uci_move_for(start_square, target_square, None);
+            self.model.board.apply_uci_move(&uci_move);
+        }
+
+        self.model.dnd_data = None;
+    }
+
+    fn handle_mouse_drag(&mut self, event: EventMotion) {
+        let (x, y) = event.position();
+        let cells_size = self.common_size() as f64 * 0.111;
+        let col = ((x - cells_size * 0.5) / cells_size).floor() as i16;
+        let row = ((y - cells_size * 0.5) / cells_size).floor() as i16;
+        let file = if self.model.reversed { 7 - col } else { col };
+        let rank = if self.model.reversed { row } else { 7 - row };
+
+        match self.model.dnd_data {
+            Some(ref mut dnd_data) => {
+                dnd_data.x = x;
+                dnd_data.y = y;
+
+                let in_bounds = file >= 0 && file <= 7 && rank >= 0 && rank <= 7;
+
+                if in_bounds {
+                    dnd_data.target_file = file as u8;
+                    dnd_data.target_rank = rank as u8;
+                }
+            }
+            _ => {}
+        };
     }
 
     fn common_size(&self) -> i32 {
