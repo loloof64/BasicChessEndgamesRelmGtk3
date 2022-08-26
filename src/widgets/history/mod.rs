04777103ab0534fe5a9d9661dda886d2e7a71136
loloof64 <laurent.bernabe@gmail.com>
2022-08-26 10:36:21 +0200

    Adding move numbers to history component.

--- src/widgets/history/mod.rs
+++ src/widgets/history/mod.rs
@@ -20,7 +20,7 @@ impl Widget for History {
 
     fn update(&mut self, event: Msg) {
         match event {
-            Msg::AddMoveSan(san, white_player) => self.add_move_san(utils::san_to_fan(san, white_player)),
+            Msg::AddMoveSan(san, white_player) => self.add_move_san(utils::san_to_fan(san, white_player), white_player),
         }
     }
 
@@ -28,6 +28,7 @@ impl Widget for History {
         Model {
             relm: relm.clone(),
             game_in_progress: false,
+            move_number: 1,
         }
     }
 
@@ -37,11 +38,15 @@ impl Widget for History {
         let provider = CssProvider::new();
         provider.load_from_data(style).unwrap();
         style_context.add_provider(&provider, STYLE_PROVIDER_PRIORITY_APPLICATION);
+        
+        //TODO only on new game
+        self.add_move_number();
+        self.widgets.root.show_all();
     }
 }
 
 impl History {
-    fn add_move_san(&mut self, san: String) {
+    fn add_move_san(&mut self, san: String, white_player: bool) {
         let button = gtk::Button::with_label(&san);
         let style_context = button.style_context();
         style_context.add_class("move_button");
@@ -50,7 +55,25 @@ impl History {
         provider.load_from_data(style).unwrap();
         style_context.add_provider(&provider, STYLE_PROVIDER_PRIORITY_APPLICATION);
         self.widgets.root.insert(&button, -1);
+
+        if !white_player {
+            self.model.move_number += 1;
+            self.add_move_number();
+        }
+
         self.widgets.root.show_all();
+    }
+
+    fn add_move_number(&mut self) {
+        let button_label = format!("{}", self.model.move_number);
+        let button = gtk::Button::with_label(&button_label);
+        let style_context = button.style_context();
+        style_context.add_class("number_button");
+        let style = include_bytes!("./style.css");
+        let provider = CssProvider::new();
+        provider.load_from_data(style).unwrap();
+        style_context.add_provider(&provider, STYLE_PROVIDER_PRIORITY_APPLICATION);
+        self.widgets.root.insert(&button, -1);
     }
 }
 
@@ -62,4 +85,5 @@ pub enum Msg {
 pub struct Model {
     relm: Relm<History>,
     game_in_progress: bool,
+    move_number: u16,
 }
